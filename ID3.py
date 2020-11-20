2c707823b8804c6fdbea0e660dbd55a01d93bfd5
Emil Khshiboun <emile.khshiboun@gmail.com>
2020-11-20 17:26:16 +0200

    Added Fifa19 dataset Implemented ID3 with stochastic support Tested on Fifa19 dataset:  \tID3 {min_leaf=100, stochastic=False} accuracy ~ 92%  ID3 {min_leaf=100, stochastic=True} accuracy ~ 89  TODO: tune min_leaf parameter.

--- ID3.py
+++ ID3.py
@@ -1,5 +1,5 @@
 import enum
-
+from tqdm import tqdm
 import numpy as np
 
 
@@ -9,9 +9,11 @@ class FeatureType(enum.Enum):
 
 
 class Feature:
-    index: int
-    type: FeatureType
-    domain: np.ndarray
+    def __init__(self, index, type, domain=[], used=False):
+        self.index = index
+        self.type = type
+        self.domain = domain
+        self.used = False
 
 
 class ID3:
@@ -46,7 +48,7 @@ class ID3:
             if val >= self.split_val:
                 return self.pointers['ge']
             else:
-                return val < self.pointers['less']
+                return self.pointers['less']
 
     def __init__(self, min_leaf_samples: int, stochastic: bool = False):
         self.features_types = np.array([])
@@ -54,96 +56,129 @@ class ID3:
         self.stochastic = stochastic
         self.root = None
 
-    def Entropy(self, samples: np.ndarray):
-        if not len(samples):
+    def Entropy(self, x_train: np.ndarray, y_train: np.ndarray):
+        if not len(x_train):
             print("#samples = 0!")
-            return None
-        classes = np.unique(samples[:, -1])
+            return +np.inf  # no
+        classes = np.unique(y_train.flatten())
         sum = 0
+        n_samples = len(x_train)
+        flattened_y = y_train.flatten()
         for i in classes:
-            p = ((samples[:, -1] == i).sum()) / len(samples)
+            p = ((flattened_y == i).sum()) / n_samples
             if p > 0:
                 sum += (p * np.log2(p))
         return -sum
 
-    def _get_IG(self, feature: Feature, samples_in_node: np.ndarray):
-        E = len(samples_in_node)
-        entropy = self.Entropy(samples_in_node)
+    def _get_IG(self, feature: Feature, x_train: np.ndarray, y_train: np.ndarray):
+        E = len(x_train)
+        entropy = self.Entropy(x_train, y_train)
 
         if feature.type == FeatureType.Discrete:  # Discrete values
-            values = samples_in_node[:, feature.index]
+            values = np.unique(x_train[:, feature.index])
             information_gain = entropy
             for value in values:
-                mask = samples_in_node[:, feature.index] == value
-                E_i = samples_in_node[mask, :]
-                E_i_entropy = self.Entropy(E_i)
-                information_gain -= (len(E_i) / E) * E_i_entropy
+                mask = x_train[:, feature.index] == value
+                x_i = x_train[mask, :]
+                y_i = y_train[mask, :]
+                E_i_entropy = self.Entropy(x_i, y_i)
+                information_gain -= (len(x_i) / E) * E_i_entropy
             return information_gain, None
         else:  # Continuous values
-            values = samples_in_node[:, feature.index]
+            v = np.sort(np.unique(x_train[:, feature.index]))
+            values = [(v[i] + v[i + 1]) / 2 for i in range(len(v) - 1)]
             max_information_gain = -np.inf
             split_value = None
             for value in values:
-                less_mask = samples_in_node[:, feature.index] < value
-                less_samples = samples_in_node[less_mask, :]
-                ge_mask = samples_in_node[:, feature.index] >= value  # greater equal than value samples.
-                ge_samples = samples_in_node[ge_mask, :]
-                child_entropy = (len(less_samples) / E) * self.Entropy(less_samples)
-                child_entropy += (len(ge_samples) / E) * self.Entropy(ge_samples)
+                less_mask = x_train[:, feature.index] < value
+                less_x_train = x_train[less_mask, :]
+                less_y_train = y_train[less_mask, :]
+                ge_mask = x_train[:, feature.index] >= value  # greater equal than value samples.
+                ge_x_train = x_train[ge_mask, :]
+                ge_y_train = y_train[ge_mask, :]
+
+                child_entropy = (len(less_x_train) / E) * self.Entropy(less_x_train, less_y_train)
+                child_entropy += (len(ge_x_train) / E) * self.Entropy(ge_x_train, ge_y_train)
                 information_gain = entropy - child_entropy
-                max_information_gain = information_gain if information_gain > max_information_gain else max_information_gain
                 split_value = value if information_gain > max_information_gain else split_value
+                max_information_gain = information_gain if information_gain > max_information_gain else max_information_gain
+
             return max_information_gain, split_value
 
-    def build_recursively(self, E: np.ndarray, features: list[Feature], default_clf):
-        if len(E) == 0:
+    def _getNextFeature(self, x_train: np.ndarray, y_train: np.ndarray, features: np.ndarray):
+        info_gains = np.zeros(shape=features.shape)
+        split_val = np.zeros(shape=features.shape)
+        for i, feature in enumerate(features):
+            if feature.type == FeatureType.Discrete and feature.used: continue
+            info_gains[i], split_val[i] = self._get_IG(feature, x_train, y_train)
+
+        info_gains[info_gains < 0] = 0
+        info_sum = sum(info_gains)
+        if info_sum == 0.0:
+            print("info_sum is zero")
+        if self.stochastic:
+            if info_sum == 0:
+                print("it's 0, yeah... stochastics")
+            P = np.array([info_gains[i] / info_sum for i in range(len(info_gains))])
+            idx = np.random.choice(np.arange(0, len(info_gains)), p=P)
+        else:
+            idx = info_gains.argmax()
+
+        return features[idx], split_val[idx]
+
+    def build_recursively(self, x_train: np.ndarray, y_train: np.ndarray, features, default_clf):
+        if len(x_train) == 0:
             return self.Node(is_leaf=True, clf=default_clf)
-        clf = np.bincount(E[:, -1]).argmax()
-        if len(E) < self.min_leaf_samples:
+        clf = np.bincount(y_train.flatten()).argmax()
+        if len(x_train) < self.min_leaf_samples:
             return self.Node(is_leaf=True, clf=clf)
-        if len(features) == 0 or len(np.unique(E[:, -1])) == 1:
+        if len(features) == 0 or len(np.unique(y_train.flatten())) == 1:
             return self.Node(is_leaf=True, clf=clf)
-        f, f_ig, split_val = None, -np.inf, None
 
-        for feature in features:
-            ig, val = self._get_IG(feature, E)
-            f = feature if (ig > f_ig) else f
-            split_val = val if (ig > f_ig) else split_val
-            f_ig = ig if (ig > f_ig) else f_ig
-
+        f, split_val = self._getNextFeature(x_train, y_train, features)
         if f.type == FeatureType.Discrete:
-            np.delete(features, f)
+            f.used = True
         node = self.Node(clf, feature=f)
         if f.type == FeatureType.Discrete:
             for d in f.domain:
-                child_samples_mask = E[:, f.index] == d
-                child_samples = E[child_samples_mask, :]
-                child_node = self.build_recursively(E=child_samples, features=features, default_clf=clf)
+                child_samples_mask = x_train[:, f.index] == d
+                child_x = x_train[child_samples_mask, :]
+                child_y = y_train[child_samples_mask, :]
+                child_node = self.build_recursively(x_train=child_x, y_train=child_y, features=features,
+                                                    default_clf=clf)
                 node.addChild(d, child_node)
         else:
-            left_child_mask = E[:, f.index] < split_val
-            right_child_mask = E[:, f.index] >= split_val
-            left_child_samples = E[left_child_mask, :]
-            right_child_samples = E[right_child_mask, :]
-            left_node = self.build_recursively(E=left_child_samples, features=features, default_clf=clf)
-            right_node = self.build_recursively(E=right_child_samples, features=features, default_clf=clf)
+            left_child_mask = x_train[:, f.index] < split_val
+            right_child_mask = x_train[:, f.index] >= split_val
+            left_child_x = x_train[left_child_mask, :]
+            left_child_y = y_train[left_child_mask, :]
+            right_child_x = x_train[right_child_mask, :]
+            right_child_y = y_train[right_child_mask, :]
+            left_node = self.build_recursively(x_train=left_child_x, y_train=left_child_y, features=features,
+                                               default_clf=clf)
+            right_node = self.build_recursively(x_train=right_child_x, y_train=right_child_y, features=features,
+                                                default_clf=clf)
             node.addChild('less', left_node)
             node.addChild('ge', right_node)
             node.split_val = split_val
         return node
 
-    def fit(self, samples: np.ndarray, features: list[Feature]):
-        clf = np.bincount(E[:, -1]).argmax()
-        self.root = self.build_recursively(samples, features, clf)
+    def fit(self, x_train: np.ndarray, y_train: np.ndarray, features):
+        clf = np.bincount(y_train.flatten()).argmax()
+        self.root = self.build_recursively(x_train, y_train, features, clf)
 
     def predict_(self, sample):
-        current_node: ID3.Node = root
+        current_node: ID3.Node = self.root
         while not current_node.isLeaf():
-            current_node = current_node.travel(sample)
+            next_node = current_node.travel(sample)
+            if next_node == None:
+                return current_node.getClassification()
+            current_node = next_node
         return current_node.getClassification()
 
     def predict(self, samples: np.ndarray):
         prediction = np.empty(shape=samples.shape[0])
-        for i, sample in enumerate(samples):
+        for i, sample in tqdm(enumerate(samples)):
             prediction[i] = self.predict_(sample)
+
         return prediction
--- dataloader.py
+++ dataloader.py
@@ -1,21 +1,41 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-
-class DataLoader:
-
-    def __init__(self, filename):
-        csv = pd.read_csv(filename, index_col=0)
-        print(csv)
-        ss = csv.iloc[:, -1].to_numpy()
-        print(ss.dtype)
-
-        narray = csv.to_numpy()
-        ss1 = narray[:,-1]
-        print(ss1.dtype)
-
-
-
+import ID3
 
 if __name__ == "__main__":
-    dl = DataLoader("datasets/fifa19/new_data.csv")
+    csv = pd.read_csv("datasets/fifa19/players.csv",index_col=0)
+    x_train, test = train_test_split(csv, test_size=(1 / 3), shuffle=False)
+    # x_train = csv
+    # test = pd.read_csv("datasets/236501/test.csv")
+    features = np.array([ID3.Feature(index=i, type=ID3.FeatureType.Continuous) for i in range(csv.shape[1] - 1)])
+    features[1] = ID3.Feature(index=1, type=ID3.FeatureType.Discrete, domain=list(range(26)))
+
+    dt = ID3.ID3(100,stochastic=True)
+    x_train = x_train.to_numpy()
+    test = test.to_numpy()
+    y_train: np.ndarray = x_train[:, -1]
+    y_train = y_train.reshape(-1, 1)
+    y_train = y_train.astype(int, copy=False)
+    x_train = np.delete(x_train, -1, axis=1)
+
+    y_test: np.ndarray = test[:, -1]
+    y_test = y_test.reshape(-1, 1)
+    y_test = y_test.astype(int, copy=False)
+    x_test = np.delete(test, - 1, axis=1)
+
+    print(csv)
+    dt.fit(x_train, y_train, features)
+
+
+
+    predicts = dt.predict(x_test)
+    y_test = y_test.flatten()
+    correct = 0
+    print(len(predicts),"   ",len(y_test))
+    for i in range(len(y_test)):
+        if (predicts[i] == y_test[i]):
+            correct += 1
+    accuracy = (correct/len(y_test))*100
+    print(accuracy)
